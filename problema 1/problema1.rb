7d6377771d22e13f88d7015a0e9d783490bccea7
duvan barrientos <duvanbelo@gmail.com>
2012-02-07 14:06:49 -0500

    solucion problema 1

---
+++ problema 1/problema1.rb
@@ -0,0 +1,15 @@
+puts "ingrese un numero entero"
+a  = gets.to_i
+
+	if a != 0 then
+		if a > 0 then
+			puts "numero positivo"
+			else
+			puts "numero negativo"
+         	end
+		else
+		puts "numero neutro"
+	end 
+
+
+
