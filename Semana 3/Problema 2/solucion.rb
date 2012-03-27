160439ef48d1987df6d8878d7fe34f1650f89513
Ruben Espinosa <rderoldan1@gmail.com>
2012-03-27 14:24:00 -0500

    adding solucion problema 2

--- Semana 3/Problema 2/solucion.rb
+++ Semana 3/Problema 2/solucion.rb
@@ -1,21 +1,20 @@
 class Solucion
    def run
-      puts "ingrese numero inicial y final separados por un espacio"
-      numeros = gets.chomp
-      if numeros.eql? "exit"
-         exit!
-      else
-        numeros = numeros.split()
-        if valido?(numeros[0].to_i,numeros[1].to_i)
-          for i in (numeros[0].to_i..numeros[1].to_i)
-            for j in (0..10)
-              puts "#{i} * #{j} = #{i*j}"
-            end
+      loop do
+        puts "ingrese numero inicial y final separados por un espacio"
+        numeros = gets.chomp
+        break if numeros.eql? "exit"
+          numeros = numeros.split()
+          if valido?(numeros[0].to_i,numeros[1].to_i)
+            for i in (numeros[0].to_i..numeros[1].to_i)
+              for j in (0..10)
+                puts "#{i} * #{j} = #{i*j}"
+              end
 
+            end
+          else
+            puts "el primer numero debe ser mayor e.j:  2 6"
           end
-        else
-          puts "el primer numero debe ser mayor e.j:  2 6"
-        end
       end
    end
 
