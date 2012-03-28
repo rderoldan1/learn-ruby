7ef30cd5e9cfcb11bd0181321496ff9babbe8b30
Ruben Espinosa <rderoldan1@gmail.com>
2012-03-27 22:10:47 -0500

    adding solucion problem 2 week 4

--- Semana 4/Problema 2/solucion.rb
+++ Semana 4/Problema 2/solucion.rb
@@ -1,3 +1,17 @@
 class Solucion
-
+  def run
+    suma = 0
+    for i in 1..100
+      dado_1 = 1 + rand(6)
+      dado_2 = 1 + rand(6)
+      puts "Dado 1 = #{dado_1}; Dado 2 = #{dado_2}"
+      if dado_1 + dado_2 == 10
+        suma += 1
+      end
+    end
+    puts "El numero de veces que salio 10 fue #{suma}"
+  end
 end
+
+solucion = Solucion.new
+solucion.run
