4963a3a3745c12ffcc81b9b7e00a588128aada11
James Benitez <michinbenitez@gmail.com>
2012-03-28 08:31:59 -0500

    cambios

--- Semana 4/Problema 1/aleatorio.rb
+++ Semana 4/Problema 1/aleatorio.rb
@@ -1,36 +1,42 @@
-def cara(numero)
-  total =0
-  total1=0
-     while numero>0  do
-       ok = rand(2)
-
-       if ok==0
-           puts "cara"
-           total +=1
-       else ok==1
-         puts "sello"
-          total1 +=1
-       end
-       numero -=1
-
-     end
-      if (total == total1)
-        puts "ocurrio un empate #{total} = #{total1}"
-      else
-     puts "TOTAL caras #{total}, contra #{total1} sellos"
-      end
-end
-
-
 class Juego
-  loop do
-    puts "entre juegos"
-    juegos = gets.chomp.to_i
+    def cara(numero)
+      total =0
+      total1=0
+         while numero>0  do
+           ok = rand(2)
 
-    if juegos ==0
-      puts "introduzca un numero valido "
-    else
-      cara(juegos)
+           if ok==0
+               puts "cara"
+               total +=1
+           else ok==1
+             puts "sello"
+              total1 +=1
+           end
+           numero -=1
+
+         end
+          if (total == total1)
+            puts "ocurrio un empate #{total} = #{total1}"
+          else
+         puts "TOTAL caras #{total}, contra #{total1} sellos"
+          end
     end
-  end
+
+
+
+    def correr
+      loop do
+        puts "entre juegos"
+        juegos = gets.chomp.to_i
+
+        if juegos ==0
+          puts "introduzca un numero valido "
+        else
+          cara(juegos)
+        end
+      end
+    end
 end
+
+juego = Juego.new
+juego.correr
